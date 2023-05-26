9af7c8a4308e5d29aa4d0d2806f9677ef223c037
murtaza.aslani <murtaza.aslani1@gmail.com>
2023-05-27 00:24:49 +0430

    This code is about fibonochi in Exam

---
+++ Fib/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fib
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 3;
+            int fib1 = 0, fib2 = 1;
+
+            Console.Write(fib1 + "\t");
+            Console.Write(fib2 + "\t");
+
+            for (int i = 0; i < n; i++)
+            {
+                int fibNext = fib1 + fib2;
+                Console.Write(fibNext + "\t");
+
+                fib1 = fib2;
+                fib2 = fibNext;
+            }
+
+            Console.ReadLine();
+        }
+    }
+
+}
